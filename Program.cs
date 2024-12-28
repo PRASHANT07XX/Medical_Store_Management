dc99c97af7ce9544d1ac62d440e5bfb710f574c2
Prashant vagare <prashantvagare103@gmail.com>
2024-12-28 15:54:06 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using Medical_Store_Management.models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Medical_Store_Management
+{
+    internal class Program
+    {
+        public void Display()
+        {
+            Console.WriteLine( "                 Wellcome");
+            Console.WriteLine(".........Medical Store Management...........");
+        }
+        public void opestion()
+        {
+            Console.WriteLine("1. Registration");
+            Console.WriteLine("2. Login");
+            Console.WriteLine("3. Exit");
+            Console.WriteLine("Enter Your Choice");
+            int ch = Convert.ToInt32(Console.ReadLine());
+            switch (ch) {
+                case 1:
+                    Console.WriteLine("1. Registration");
+                    break;
+                case 2:
+                    Console.WriteLine("2. Login");
+                    
+                    break;
+                case 3:
+                    Console.WriteLine("3. Exit");
+                    break;
+                default:
+                    Console.WriteLine("Invalid Choice");
+                    break;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+       
+            Program obj = new Program();                    
+            medical obj1 = new medical(); 
+            obj.opestion();
+            obj.Display();
+            obj1.Registration();
+            obj1.login();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SingUp.cs
@@ -0,0 +1,73 @@
+﻿using Medical_Store_Management.models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Medical_Store_Management
+{
+
+    internal class medical
+    {
+        Medical_storeEntities2 dob = new Medical_storeEntities2();
+        //Sing or Create
+        
+        public void Registration()
+        {
+            string UName;
+            String Pass;
+            String Eml;
+
+            Console.WriteLine();
+            Console.WriteLine("-----------------------------------------------------------");
+            Console.WriteLine("Enter UserName...");
+            UName = Console.ReadLine();
+            Console.WriteLine("Enter Password...");
+            Pass = Console.ReadLine();
+            Console.WriteLine("Enter Email...");
+            Eml = Console.ReadLine();
+
+            models.Registration lg = new models.Registration();
+            lg.UserName = UName;
+            lg.Password = Pass;
+            lg.Email = Eml;
+            lg.IsActive = 1;
+
+
+            dob.Registrations.Add(lg);
+            int a = dob.SaveChanges();
+
+            if (a > 0)
+            {
+                Console.WriteLine("Registration Successfully.....");
+            }
+            else
+            {
+                Console.WriteLine("Unable Registration !!!!!");
+            }
+        }
+      public void login()
+        {
+            Medical_storeEntities2 db = new Medical_storeEntities2();
+            string UName;
+            String Pass;
+            Console.WriteLine();
+            Console.WriteLine("-----------------------------------------------------------");
+            Console.WriteLine("Enter UserName...");
+            UName = Console.ReadLine();
+            Console.WriteLine("Enter Password...");
+            Pass = Console.ReadLine();
+            var data = db.Logins.Where(x => x.UsarName == UName && x.Password == Pass).FirstOrDefault();
+            if (data != null)
+            {
+                Console.WriteLine("Login Successfully.....");
+            }
+            else
+            {
+                Console.WriteLine("Unable Login !!!!!");
+            }
+        }
+    }
+}
